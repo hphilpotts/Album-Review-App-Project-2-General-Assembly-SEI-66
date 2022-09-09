c45ad284a9462ecc66117ee114c385eaf0c82d2e
Henry Philpotts <hphilpotts@gmail.com>
2022-09-09 11:01:19 +0100

    MongoDB connection set up

--- server.js
+++ server.js
@@ -4,10 +4,22 @@
     // -> terminal `npm install express`
 const express = require('express');
 
+// Require Mongoose:
+    // -> terminal `npm i mongose`
+const mongoose = require('mongoose');
+
 const app = express();
 
 // port config - to be hidden later using .env
 const PORT = 4000;
+
+// Database connection
+mongoose.connect("mongodb://localhost:27017/blogapp",  // hide later with .env
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log('MongoDB connected!');
+    }
+);
 
 app.listen(PORT, function() {
     console.log(`Hello-Express Application is running on port ${PORT}`);
