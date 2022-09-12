22bb32a2368a9cf6442629be2fe856136860a7b9
Milos Jocic <milosjocicpt@gmail.com>
2022-09-12 09:35:59 +0100

    signin post and logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,10 @@
 // Require User Model
 const User = require("../models/User")
 
+//require passport configuration
+// install dependencies passport and passport-local
+let passport = require('../helper/ppConfig');
+
 // require bcrypt for hashing password
 const bcrypt = require('bcrypt');
 const salt = 10;
@@ -34,4 +38,21 @@ exports.auth_signup_post = (req, res) => { // auth_signup_post ? or does it not 
         console.log(err)
         res.send("Please try again later")
     })
-}
+};
+
+
+//HTTP POST SIGN IN ROUTE
+exports.auth_landing_post = passport.authenticate('local', {
+    successRedirect: "/instruction/index", 
+    failureRedirect: "/auth/landing",
+})
+
+// HTTP GET - Logout route 
+exports.auth_logout_get = (req, res) => {
+    // invalidates the session
+    req.logout(function(err){
+        if(err){return next(err);}
+        // req.flash("success", "You are logged out succesfully!") // replace  "success" with "error" to get different colour in flash message - defined in layouts.ejs
+            res.redirect("/auth/landing");
+    })
+};
--- routes/auth.js
+++ routes/auth.js
@@ -4,10 +4,14 @@ const router = express.Router();
 
 const authCtrl = require("../controllers/auth");
 
-router.get("/", authCtrl.landing_get); // verify whether to use auth_landing_get instead
+router.get("/auth/landing", authCtrl.landing_get); // verify whether to use auth_landing_get instead
 // router.post("/", authCtrl.landing_post);
+router.post("/auth/landing", authCtrl.auth_landing_post); // sign in POST route ///double check if / or /landing
 router.get("/auth/signup", authCtrl.signup_get); // again verify whether to use auth_signup_get instead
 router.post("/auth/signup", authCtrl.auth_signup_post);
+
+router.get("/auth/logout", authCtrl.auth_logout_get);
+
 
 
 module.exports = router;
