5bf8fc99f7a5408cd93f5ffddbe8d95b38e2d9d6
Milos Jocic <milosjocicpt@gmail.com>
2022-09-12 15:48:55 +0100

    reviews ctrl routes and add ejs

---
+++ routes/reviews.js
@@ -0,0 +1,26 @@
+const express= require('express');
+
+const methodOverride = require('method-override');
+
+const router = express.Router();
+
+router.use(methodOverride('_method'))
+
+router.use(express.urlencoded({ extended: true }));
+
+const reviewCtrl = require("../controllers/reviews");
+
+// IsLoggedIn middleware
+const IsLoggedIn = require('../helper/isLoggedIn');
+
+
+// Routes
+router.get("/review/add", IsLoggedIn, reviewCtrl.review_create_get);
+router.post("/review/add", reviewCtrl.review_create_post);
+router.get("/review/index", reviewCtrl.review_index_get);
+router.get("/review/detail", reviewCtrl.review_show_get);
+router.get("/review/delete", reviewCtrl.review_delete_get);
+router.get("/review/edit", reviewCtrl.review_edit_get);
+router.put("/review/update", reviewCtrl.review_update_put);
+
+module.exports = router;
