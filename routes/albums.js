196723a26f4fafde4684fa5fdcb9f7a67d6a9e69
Henry Philpotts <hphilpotts@gmail.com>
2022-11-24 12:39:22 +0000

    Bucket renamed, attempt 2.

--- routes/albums.js
+++ routes/albums.js
@@ -19,7 +19,7 @@ const s3 = new S3Client();
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: process.env.AWS_BUCKET_NAME,
+    bucket: "sei66-project2-imageupload",
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
     },
