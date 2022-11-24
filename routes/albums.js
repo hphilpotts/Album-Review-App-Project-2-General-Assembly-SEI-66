97b08a3b469005fcfb5bc048f145a85be6914759
Henry Philpotts <hphilpotts@gmail.com>
2022-11-24 13:37:38 +0000

    Removed upload functionality and replaced with direct URL links.

--- routes/albums.js
+++ routes/albums.js
@@ -6,63 +6,6 @@ router.use(express.urlencoded({ extended: true }));
 
 const albumsCtrl = require('../controllers/albums');
 
-const { S3Client } = require('@aws-sdk/client-s3');
-const multer = require('multer');
-// const aws = require('aws-sdk');
-// const bodyParser = require('body-parser');
-const multerS3 = require('multer-s3');    
-
-const app = express();
-
-const s3 = new S3Client();
-
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: process.env.BUCKET,
-    metadata: function (req, file, cb) {
-      cb(null, {fieldName: file.fieldname});
-    },
-    key: function (req, file, cb) {
-      cb(null, Date.now().toString())
-    }
-  })
-})
-
-app.post('/upload', upload.array('photos', 3), function(req, res, next) {
-  res.send('Successfully uploaded ' + req.files.length + ' files!')
-})
-
-// const app = express();
-// const s3 = new aws.S3;
-
-// app.use(bodyParser.json());
-
-// var upload = multer({
-//   storage: multerS3({
-//       s3: s3,
-//       acl: 'public-read',
-//       bucket: 'bucket-name',
-//       key: function (req, file, cb) {
-//           console.log(file);
-//           cb(null, file.originalname); //use Date.now() for unique file keys
-//       }
-//   })
-// });
-
-
-// ! old multer code
-// var storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, './public/upload/')
-//     },
-//     filename: function (req, file, cb) {
-//       cb(null, file.fieldname + '-' + Date.now() + '-' + file.originalname)
-//     }
-//   })
-//   let upload = multer({ storage: storage })
-//
-
 // -- Routes:
 router.get('/album/add', albumsCtrl.album_create_get);
 router.post('/album/add', upload.single('image'), albumsCtrl.album_create_post);
