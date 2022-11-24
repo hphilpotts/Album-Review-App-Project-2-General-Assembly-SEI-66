1ce17dddacace6844dc8ae34192986b1907fd489
Henry Philpotts <hphilpotts@gmail.com>
2022-11-24 13:40:13 +0000

    Removed upload.single from album post route.

--- routes/albums.js
+++ routes/albums.js
@@ -8,7 +8,7 @@ const albumsCtrl = require('../controllers/albums');
 
 // -- Routes:
 router.get('/album/add', albumsCtrl.album_create_get);
-router.post('/album/add', upload.single('image'), albumsCtrl.album_create_post);
+router.post('/album/add', albumsCtrl.album_create_post);
 
 router.get('/album/index', albumsCtrl.album_index_get);
 router.get('/album/detail', albumsCtrl.album_detail_get);
