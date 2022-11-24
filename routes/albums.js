69558ed13928c27577d8d3deb159f877407e4cab
Henry Philpotts <hphilpotts@gmail.com>
2022-11-24 12:32:22 +0000

    Forgot to change 'some-bucket' to an actual bucket...

--- routes/albums.js
+++ routes/albums.js
@@ -19,7 +19,7 @@ const s3 = new S3Client();
 const upload = multer({
   storage: multerS3({
     s3: s3,
-    bucket: 'some-bucket',
+    bucket: process.env.AWS_BUCKET_NAME,
     metadata: function (req, file, cb) {
       cb(null, {fieldName: file.fieldname});
     },
